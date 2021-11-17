c9a2d07941752e1f2f95a8f83afda623bbfcf759
Yun-Sheng Chang <yschang@mit.edu>
2021-11-17 14:53:52 -0500

    Add `scripts/loc.sh` to count LOC

---
+++ scripts/loc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIRS="txn tuple index gc cfmutex config"
+FILES_EXEC=$(find ${DIRS} | grep -E ".go$" | grep -vE "_test.go$" | tr '\n' ' ')
+FILES_TEST=$(find ${DIRS} | grep -E "_test.go$" | tr '\n' ' ')
+
+echo "Executable files:"
+wc -l $FILES_EXEC
+
+echo "Testing files:"
+wc -l $FILES_TEST
+
