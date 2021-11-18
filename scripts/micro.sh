aeb95096c0d255f08ec4a9ea4d9c7f4fe18abdc8
Yun-Sheng Chang <yschang@mit.edu>
2021-11-17 20:28:57 -0500

    Update `micro.sh` to 60 cores

--- scripts/micro.sh
+++ scripts/micro.sh
@@ -5,27 +5,29 @@ rm -rf $dir
 mkdir $dir
 
 fpath=$dir/micro-read.csv
-for rkeys in 1000 10000 100000
+for rkeys in 1000 100000
 do
 	for nkeys in 1 10 100
 	do
-		for nthrds in 1 2 3 4 5 6 7 8 9
+		for nthrds in $(seq 60)
 		do
 			echo "rkeys = $rkeys; nkeys = $nkeys; nthrds = $nthrds"
-			stdbuf -o 0 perflock numactl --cpunodebind=0 go run ./benchmark/micro-read/ -rkeys $rkeys -nkeys $nkeys -nthrds $nthrds -exp | tee -a $fpath
+			#stdbuf -o 0 perflock numactl --cpunodebind=0 go run ./benchmark/micro-read/ -rkeys $rkeys -nkeys $nkeys -nthrds $nthrds -exp | tee -a $fpath
+			stdbuf -o 0 perflock numactl --physcpubind=+0-$nthrds go run ./benchmark/micro-read/ -rkeys $rkeys -nkeys $nkeys -nthrds $nthrds -exp | tee -a $fpath
 		done
 	done
 done
 
 fpath=$dir/micro-write.csv
-for rkeys in 1000 10000 100000
+for rkeys in 1000 100000
 do
 	for nkeys in 1 10 100
 	do
-		for nthrds in 1 2 3 4 5 6 7 8 9
+		for nthrds in $(seq 60)
 		do
 			echo "rkeys = $rkeys; nkeys = $nkeys; nthrds = $nthrds"
-			stdbuf -o 0 perflock numactl --cpunodebind=0 go run ./benchmark/micro-write/ -rkeys $rkeys -nkeys $nkeys -nthrds $nthrds -exp | tee -a $fpath
+			#stdbuf -o 0 perflock numactl --cpunodebind=0 go run ./benchmark/micro-write/ -rkeys $rkeys -nkeys $nkeys -nthrds $nthrds -exp | tee -a $fpath
+			stdbuf -o 0 perflock numactl --physcpubind=+0-$nthrds go run ./benchmark/micro-write/ -rkeys $rkeys -nkeys $nkeys -nthrds $nthrds -exp | tee -a $fpath
 		done
 	done
 done
