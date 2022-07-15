520121677860f1b007ebd2944bc3341e4c8f3a7f
Yun-Sheng Chang <yschang@mit.edu>
2022-07-15 15:37:05 -0400

    Update genTID

--- txn/txn.go
+++ txn/txn.go
@@ -93,8 +93,18 @@ func (txnMgr *TxnMgr) New() *Txn {
 
 func genTID(sid uint64) uint64 {
 	var tid uint64
+
+	/* Call `GetTSC` and round the result up to site ID boundary. */
 	tid = grove_ffi.GetTSC()
-	tid = (tid & ^(config.N_TXN_SITES - 1)) + sid
+	tid = ((tid + config.N_TXN_SITES) & ^(config.N_TXN_SITES - 1)) + sid
+	// Below is the old (and wrong) version where we simply round the result,
+	// up or down, to site ID boundary.
+	// tid = (tid & ^(config.N_TXN_SITES - 1)) + sid
+
+	/* Wait until TSC exceeds TID. */
+	for grove_ffi.GetTSC() <= tid {
+	}
+
 	return tid
 }
 
