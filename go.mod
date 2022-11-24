e0e030654d237fc639198af68f0bbf10f548718d
Ralf Jung <research@ralfj.de>
2022-11-24 20:35:40 +0100

    gentid: assume no-overflow and simplify code

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mit-pdos/go-mvcc
 go 1.15
 
 require (
+	github.com/goose-lang/std v0.0.0-20220414201102-c41554454045
 	github.com/mit-pdos/gokv v0.0.0-20220720182433-44b23d6431c9
 	github.com/stretchr/testify v1.7.0
 	github.com/tchajed/goose v0.5.1-0.20220831163625-b0c9a5787a1a
--- tid/tid.go
+++ tid/tid.go
@@ -1,6 +1,7 @@
 package tid
 
 import (
+	"github.com/goose-lang/std"
 	"github.com/mit-pdos/gokv/grove_ffi"
 	"github.com/mit-pdos/go-mvcc/config"
 )
@@ -10,7 +11,7 @@ func GenTID(sid uint64) uint64 {
 
 	/* Call `GetTSC` and round the result up to site ID boundary. */
 	tid = grove_ffi.GetTSC()
-	tid = ((tid + config.N_TXN_SITES) & ^(config.N_TXN_SITES - 1)) + sid
+	tid = std.SumAssumeNoOverflow(tid,config.N_TXN_SITES)/config.N_TXN_SITES * config.N_TXN_SITES + sid
 	// Below is the old (and wrong) version where we simply round the result,
 	// up or down, to site ID boundary.
 	// tid = (tid & ^(config.N_TXN_SITES - 1)) + sid
@@ -21,4 +22,3 @@ func GenTID(sid uint64) uint64 {
 
 	return tid
 }
-
