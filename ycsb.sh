186ecf8cbf6b71dbf1b7ad3d932dbc727b892bec
Yun-Sheng Chang <yschang@mit.edu>
2022-12-07 16:29:58 -0500

    Update ycsb.sh

--- ycsb.sh
+++ ycsb.sh
@@ -4,26 +4,30 @@ dir=./exp
 # rm -rf $dir
 mkdir -p $dir
 
-cc=mvcc
-duration=60
-rkeys=10000000
+duration=10
+rkeys=100000
 
-fpath=$dir/ycsb-$cc.csv
-for rdratio in 0 10 20 30 40 50 60 70 80 90 100
+cc="mvcc"
+theta=0.8
+
+fpath=$dir/ycsb-$cc-$theta.csv
+for rdratio in 0 20 40 60 80 100
 do
 	# for nthrds in $(seq 8)
 	for nthrds in 8
 	do
-		stdbuf -o 0 go run ./benchmark/ycsb.go -nthrds $nthrds -duration $duration -rdratio $rdratio -rkeys $rkeys -exp | tee -a $fpath
+		# stdbuf -o 0 numactl --physcpubind=+20-29 go run ./benchmark/ycsb.go -nthrds $nthrds -duration $duration -rdratio $rdratio -rkeys $rkeys -theta $theta -exp | tee -a $fpath
+		stdbuf -o 0 go run ./benchmark/ycsb.go -nthrds $nthrds -duration $duration -rdratio $rdratio -rkeys $rkeys -theta $theta -exp | tee -a $fpath
 	done
 done
 
-fpath=$dir/ycsb-long-$cc.csv
-for rdratio in 0 10 20 30 40 50 60 70 80 90 100
+fpath=$dir/ycsb-long-$cc-$theta.csv
+for rdratio in 0 20 40 60 80 100
 do
 	# for nthrds in $(seq 8)
 	for nthrds in 8
 	do
-		stdbuf -o 0 go run ./benchmark/ycsb.go -nthrds $nthrds -duration $duration -rdratio $rdratio -rkeys $rkeys -long -exp | tee -a $fpath
+		# stdbuf -o 0 numactl --physcpubind=+20-29 go run ./benchmark/ycsb.go -nthrds $nthrds -duration $duration -rdratio $rdratio -rkeys $rkeys -theta $theta -long -exp | tee -a $fpath
+		stdbuf -o 0 go run ./benchmark/ycsb.go -nthrds $nthrds -duration $duration -rdratio $rdratio -rkeys $rkeys -theta $theta -long -exp | tee -a $fpath
 	done
 done
