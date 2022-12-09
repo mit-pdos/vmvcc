bcad314084df9909b0427aa4c100b1cf06628a51
Yun-Sheng Chang <yschang@mit.edu>
2022-12-09 07:39:26 -0500

    Add a closed example

---
+++ examples/hello.go
@@ -0,0 +1,28 @@
+package examples
+
+import (
+	"github.com/mit-pdos/go-mvcc/txn"
+)
+
+func hello(txn *txn.Txn) bool {
+	txn.Put(0, "hello")
+	txn.Get(0)
+	txn.Delete(0)
+
+	return true
+}
+
+func Hello(txno *txn.Txn) {
+	body := func(txni *txn.Txn) bool {
+		return hello(txni)
+	}
+	txno.DoTxn(body)
+}
+
+func CallHello() {
+	db := txn.MkTxnMgr()
+	db.ActivateGC()
+
+	txn := db.New()
+    Hello(txn)
+}
