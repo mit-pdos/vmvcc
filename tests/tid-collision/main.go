6a95955d9979a5ca79380b8bc3d88c7e79f201d1
Yun-Sheng Chang <yschang@mit.edu>
2023-07-15 13:46:58 -0400

    Move test_tid into tests

--- tests/tid-collision/main.go
+++ tests/tid-collision/main.go
@@ -1,3 +1,6 @@
+// This test intends to test "how frequent timestamps collide if without our
+// technique to make it unique". Comment out every line in tid.go except L13
+// for comparison.
 package main
 
 import (
