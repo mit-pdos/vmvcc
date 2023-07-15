6745214b82b17b8b88e94ae4b086d9f8b9fdc905
Yun-Sheng Chang <yschang@mit.edu>
2023-07-15 14:01:47 -0400

    Clean up index

--- index/index.go
+++ index/index.go
@@ -7,12 +7,12 @@ import (
 )
 
 type IndexBucket struct {
-	latch	*sync.Mutex
-	m		map[uint64]*tuple.Tuple
+	latch *sync.Mutex
+	m     map[uint64]*tuple.Tuple
 }
 
 type Index struct {
-	buckets	[]*IndexBucket
+	buckets []*IndexBucket
 }
 
 func MkIndex() *Index {
@@ -32,27 +32,25 @@ func getBucket(key uint64) uint64 {
 	// return key % config.N_IDX_BUCKET
 }
 
-/**
- * Note that `GetTuple` will always create a tuple when there is no entry in
- * `m`. This design choice seems to be wasting memory resource as we'll always
- * allocate a `Tuple` even with an empty `txn.Get`, but is actually a must: A
- * `txn.Get` should prevent earlier txns from creating new versions, even when
- * it fails to retrieve a value. More specifically, this requirement is enforced
- * with the `tidrd` field of each tuple.
- */
+// @GetTuple returns the tuple pointer associated with the key.
+//
+// @GetTuple will always create a tuple when there is no entry in @m. This
+// design choice seems to wasteful as it will allocate a tuple even with an
+// empty @txn.Get, but is actually a must: A @txn.Get should prevent earlier
+// txns from creating new versions, even when it fails to retrieve a value.
 func (idx *Index) GetTuple(key uint64) *tuple.Tuple {
 	b := getBucket(key)
 	bucket := idx.buckets[b]
 	bucket.latch.Lock()
 
-	/* Return the tuple if there exists one. */
+	// Return the tuple if there exists one.
 	tupleCur, ok := bucket.m[key]
 	if ok {
 		bucket.latch.Unlock()
 		return tupleCur
 	}
 
-	/* Create a new tuple and associate it with the key. */
+	// Create a new tuple and associate it with the key.
 	tupleNew := tuple.MkTuple()
 	bucket.m[key] = tupleNew
 
@@ -60,11 +58,12 @@ func (idx *Index) GetTuple(key uint64) *tuple.Tuple {
 	return tupleNew
 }
 
+// @getKeys returns keys in the index. Note that the return key set is merely an
+// under-approximation of the actual key set.
 func (idx *Index) getKeys() []uint64 {
 	var keys []uint64
-	/* TODO: Try a better initial cap by summing up each bucket. */
 	keys = make([]uint64, 0, 200)
-	// for b := uint64(0); b < config.N_IDX_BUCKET; b++ {
+
 	for _, bkt := range idx.buckets {
 		bkt.latch.Lock()
 		for k := range bkt.m {
@@ -72,14 +71,16 @@ func (idx *Index) getKeys() []uint64 {
 		}
 		bkt.latch.Unlock()
 	}
+
 	return keys
 }
 
+// TODO: move this out to a separate GC module.
 func (idx *Index) DoGC(tidMin uint64) {
 	keys := idx.getKeys()
+
 	for _, k := range keys {
 		tuple := idx.GetTuple(k)
 		tuple.RemoveVersions(tidMin)
 	}
 }
-
